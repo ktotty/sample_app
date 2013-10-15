ad4339e38f7e189c246bae1b5e819fc8c2c57893
Katherine Totty <kmt5092@gmail.com>
2013-10-14 19:28:10 -0500

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static Pages" do
 
+  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+
   describe "Home page" do
     it "should have the content 'Sample App'" do
     	visit '/static_pages/home'
@@ -33,5 +35,16 @@ describe "Static Pages" do
   		visit '/static_pages/about'
   		expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
   	end
+  end
+
+  describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+    end
   end
 end
