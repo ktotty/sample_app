8e2ae64cab15ab58b9cd2ad64f89f72c51318f89
Katherine Totty <kmt5092@gmail.com>
2013-12-30 20:41:05 -0600

    Add user microposts

--- app/assets/javascripts/microposts.js
+++ app/assets/javascripts/microposts.js
@@ -1,7 +1,8 @@
 function updateCountdown() {
 	// 140 character length maximum
-	var remaining = 140 - jQuery('#micropost_content').value().length;
+	var remaining = 140 - jQuery('#micropost_content').val().length;
 	jQuery('.countdown').text(remaining + ' characters remaining');
+	console.log('Here I am.');
 }
 
 jQuery(document).ready(function($) {
